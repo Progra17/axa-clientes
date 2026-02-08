03f4b3184113b79e096fba56672ceb3edbd3a26f
AndresRodriguez <al05135478@tecmilenio.mx>
2026-02-08 00:17:38 -0700

    version con errores 2

--- main.py
+++ main.py
@@ -171,15 +171,9 @@ def menu():
     while True:
         print("\n--- Sistema Axanet ---")
         print("1. Crear nuevo cliente")
-<<<<<<< HEAD
-        print("2. Consultar cliente")
-        print("3. Actualizar cliente")
-        print("4. Listar clientes")
-=======
         #print("2. Consultar cliente")
         #print("3. Actualizar cliente")
         #print("4. Listar clientes")
->>>>>>> desarrollo
         print("5. Borrar cliente")
         print("6. Registrar solicitud")
         print("7. Listar solicitudes")
@@ -189,21 +183,12 @@ def menu():
 
         if opcion == "1":
             crear_cliente()
-<<<<<<< HEAD
-        elif opcion == "2":
-            consultar_cliente()
-        elif opcion == "3":
-            actualizar_cliente()
-        elif opcion == "4":
-            listar_cliente()
-=======
         #elif opcion == "2":
         #    consultar_cliente()
         #elif opcion == "3":
         #    actualizar_cliente()
         #elif opcion == "4":
         #    listar_cliente()
->>>>>>> desarrollo
         elif opcion == "5":
             borrar_cliente()
         elif opcion == "6":
